cec5b73e96ba1e233adee85f01952b7605a2c075
Julián Melero Hidalgo <julian@melerohidalgo.es>
2020-12-07 19:47:41 +0100

    Crear clase

--- Program.cs
+++ Program.cs
@@ -4,12 +4,25 @@ namespace CursoBasicoCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static public void declaracion()
         {
             int number = 10;
             double numberDecimal = 0.0d;
             bool yayOrnay = true;
-            string hello = "This is a text!"
+            string hello = "This is a text!";
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(hello);
+
+        }
+        static void Main(string[] args)
+        {
+
+            declaracion();
+            // Llamamos a la clase
+            Song song = new Song();
+            
+            song.Title = "My title";
+
         }
     }
 }
---
+++ Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoBasicoCSharp
+{
+    class Song
+    {
+        public long Second { get; set; }
+        public string Title { get; set; }
+
+        public void Play()
+        {
+
+        }
+    }
+}
